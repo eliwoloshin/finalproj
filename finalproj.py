68fc51ad468a0b2e872b5f8b37a949f517f40af5
eliwoloshin <eliwoloshin@hanovernorwichschools.org>
2016-06-07 21:18:39 -0400

    Updated from Brython Server: 6/7/2016 9:18:39 PM

--- finalproj.py
+++ finalproj.py
@@ -1,1 +1,10 @@
+"rock paper scissors::"
 
+gametype = input("Press 1 to play the computer, press 2 to play a two player game.")
+
+if gametype == 1:
+    gametype2 = input("Press 1 if you want to make the first move, or press 2 if you want the computer to make the first move.")
+        if gametype2 == 1:
+            move1 = input("Make your move: type in rock, paper or scissors to make your move") 
+        elif gametype2 == 2:
+            ff
