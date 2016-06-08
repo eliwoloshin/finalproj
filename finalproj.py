9a10c0c9b49d6a4c6a3ea95db58e0969f4befc24
eliwoloshin <eliwoloshin@hanovernorwichschools.org>
2016-06-07 22:12:51 -0400

    Updated from Brython Server: 6/7/2016 10:12:51 PM

--- finalproj.py
+++ finalproj.py
@@ -19,7 +19,6 @@ if gametype == 1:
     gametype2 = input("Now, press 1 if you want to make the first move, or press 2 if you want the computer to make the first move. ")
     if gametype2 == 1:
         move1 = input("Make your move: type in rock, paper or scissors to make your move. ")
-        #if move1 !== "rock" and move1 !== "paper" and move1 !== "scissors":
         if move1 not in ["rock", "Rock", "paper", "Paper", "scissors", "Scissors", "scissor", "Scissor"]:
             print("Looks like you goofed that. Try again and be sure not to misspell the move you want to make.")
         if move1 == "rock":
@@ -30,10 +29,10 @@ if gametype == 1:
             print("The computer chose to put down scissors. Looks like you lost.")
         if move1 == "scissors":
                 print("The computer chose to put down rock. Looks like you lost.")
-elif gametype2 == 2:
-    name1 = input("Player 1, please enter your name.")
-    name2 = input("Player 2, please enter your name.")
-    print("Hey, "+name1+", look away from the computer so Player 1 can make his move without you seeing it.")
-    p1move1 = input("Player 1, make your move by typing in either rock, paper or scissors.")
-    print("Hey, "+name2+", look away from the computer so Player 2 can make his move without you seeing it.")
-    p2move1 = input("Player 2, make your move by typing in either rock, paper or scissors.")
+    elif gametype2 == 2:
+        name1 = input("Player 1, please enter your name.")
+        name2 = input("Player 2, please enter your name.")
+        print("Hey, "+name1+", look away from the computer so Player 1 can make his move without you seeing it.")
+        p1move1 = input("Player 1, make your move by typing in either rock, paper or scissors.")
+        print("Hey, "+name2+", look away from the computer so Player 2 can make his move without you seeing it.")
+        p2move1 = input("Player 2, make your move by typing in either rock, paper or scissors.")
